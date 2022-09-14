253363cde033e0ab8d6d888ad5aac402c0d6936d
maximalekseenko <max.alekseenko@gmail.com>
2022-09-14 15:29:45 +0300

    act On_... expended

--- components/act.py
+++ components/act.py
@@ -19,23 +19,68 @@ class Act:
         ''''''
 
 
-    # Expendable Call functions
+
+
+    # -----OPEN-----
+    def Open(self) -> None:
+        """Opens this act if closed.
+        \n (don't misstake this with expendable `On_Open`)
+        """
+        self.On_Open()
+
+
     def On_Open(self) -> None:
-        """Called upon this act's open."""
+        """Expendable.
+        \n Called when this act opens.
+        """
+
+
+    # -----CLOSE-----
+    def Close(self) -> None:
+        """Closes this act if opened.
+        \n (don't misstake this with expendable `On_Close`)
+        """
+        self.On_Close()
+
+
     def On_Close(self) -> None:
-        """Called upon this act's close."""
+        """Expendable.
+        \n Called when this act closes.
+        """
 
-    # Expendable Handle functions
-    def Tick(self) -> None:
-        """"""
 
+    # -----HANDLE-----
     def Handle(self, event:pygame.event.Event) -> None:
         """Handles pygame event when this act runs.
         \n When writing this function, pass event to all scenes, that opened.
         """
 
+        self.On_Handle(event)
 
-    def Render(self) -> None:
+
+    def On_Handle(self, event:pygame.event.Event) -> None:
+        """Handles pygame event when this act runs.
+        \n When writing this function, pass event to all scenes, that opened.
+        """
+
+    # -----RENDER-----
+    def On_Render(self) -> None:
         """Renders this act, when it runs.
         \n When writing this function, render all opened scenes.
         """
+        self.On_Render()
+
+    def On_Render(self) -> None:
+        """Renders this act, when it runs.
+        \n When writing this function, render all opened scenes.
+        """
+
+
+    # -----TICK-----
+    def Tick(self) -> None:
+        """"""
+
+    def On_Tick(self) -> None:
+        """"""
+
+
