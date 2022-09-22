59ef82305b229661a17141c9a2d3fbc37f4f74cc
maximalekseenko <max.alekseenko@gmail.com>
2022-09-22 16:07:16 +0300

    element is back (not in full power)

--- components/element.py
+++ components/element.py
@@ -8,256 +8,109 @@ from .. import components
 
 
 class Element:
-    """Basic element.
-    \n use to create Custom elements.
+    """
     """
 
 
-    def __init__(self, scene, rect) -> None:
-        """"""
+    def __init__(self, scene, rect=None, exists=True) -> None:
+        """
+        *   `self.scene = scene`
+        *   `self.rect = pygame.Rect(rect)`
+        *   `self.exists = exists`
+        """
+
         self.scene:components.Scene = scene
+        ''''''
 
-        # collision rectangle
+        if rect == None: rect = (0,0,0,0)
         self.rect:pygame.Rect = pygame.Rect(rect)
-        '''`pygame.Rect` for collision logic.'''
+        ''''''
 
-        self.On_Start()
+        self.exists:bool = exists
+        ''''''
         
 
-    # ---------- position and scale ----------
-    @property
-    def x(self) -> int: return self._x
 
-    @x.setter
-    def x(self, value:Union[int, float, str]):
-        """Distance from `parent_anchor_x` to `anchor_x`.
-        \n Can be `int`, `float` or `str%` (percent form `parent_rect`. Example: `"16%"`)
+    # -----UPDATE-----
+    def Update(self):
+        """Update this scene.
+        \n If this scene is closed, does nothing and returns `False`.
+        \n (For codding, use `On_Update`)
         """
 
-        self._x = value
-        self.Validate_x()
-        self.Math_Collision_Rectangle()        
+        # if closed
+        if not self.exists: return False
 
-    def Validate_x(self) -> None:
-        """Validate x and raise if wrong."""
-
-        if type(self._x) not in [int, float, str]: raise TypeError(f"'{self._x}' is wrong value for x. Must be int, float or str%")
-        if type(self._x) == str and (self._x[-1] != '%' or not self._x[:-1].isnumeric()): raise TypeError(f"'{self._x}' is wrong value for x. Must be int, float or str%")
+        # if opened
+        self.On_Update()
+        return True
 
     
-    @property
-    def y(self): return self._y
+    def On_Update(self):
+        """Called to update this scene.
+        \n (For calling, use `Update`)
+        """
 
-    @y.setter
-    def y(self, value:Union[int, float, str]):
-        """Distance from `parent_anchor_y` to `anchor_y`.
-        \n Can be `int`, `float` or `str%` (percent form `parent_rect`. Example: `"16%"`)
+
+    # -----TICK-----
+    def Tick(self, delta:float=0.0) -> None:
+        """Does some action for this scene.
+        \n If this scene is closed, does nothing and returns `False`.
+        \n (For codding, use `On_Tick`)
         """
         
-        self._y = value
-        self.Validate_y()
-        self.Math_Collision_Rectangle()
+        # if closed
+        if not self.exists: return False
 
-    def Validate_y(self) -> None:
-        """Validate y and raise if wrong."""
-
-        if type(self._y) not in [int, float, str]: raise TypeError(f"'{self.y}' is wrong value for y. Must be int, float or str%")
-        if type(self._y) == str and (self._y[-1] != '%' or not self._y[:-1].isnumeric()): raise TypeError(f"'{self.y}' is wrong value for y. Must be int, float or str%")
+        # if opened
+        self.On_Tick(delta)
+        return True
 
 
-    @property
-    def width(self): return self._width
-
-    @width.setter
-    def width(self, value:Union[int, float, str]):
-        """Distance from left to right.
-        \n Can be `int`, `float` or `str%` (percent form `parent_rect`. Example: `"16%"`)
-        """
-        
-        self._width = value
-        self.Validate_width()
-        self.Math_Collision_Rectangle()
-
-    def Validate_width(self) -> None:
-        """Validate width and raise if wrong."""
-        if type(self._width) not in [int, float, str]: raise TypeError(f"'{self._width}' is wrong value for width. Must be int, float or str%")
-        if type(self._width) == str and (self._width[-1] != '%' or not self._width[:-1].isnumeric()): raise TypeError(f"'{self._width}' is wrong value for width. Must be int, float or str%")
-
-
-    @property
-    def height(self): return self._height
-
-    @height.setter
-    def heigth(self, value:Union[int, float, str]):
-        """Distance from top to bottom.
-        \n Can be `int`, `float` or `str%` (percent form `parent_rect`. Example: `"16%"`)
-        """
-        
-        self._height = value
-        self.Validate_height()
-        self.Math_Collision_Rectangle()
-
-    def Validate_height(self) -> None:
-        """Validate height and raise if wrong."""
-
-        if type(self._height) not in [int, float, str]: raise TypeError(f"'{self._height}' is wrong value for height. Must be int, float or str%")
-        if type(self._height) == str and (self._height[-1] != '%' or not self._height[:-1].isnumeric()): raise TypeError(f"'{self._height}' is wrong value for height. Must be int, float or str%")
-
-  
-
-    # ---------- anchors ----------
-    @property
-    def anchor_x(self) -> str: return self._anchor_x
-
-    @anchor_x.setter
-    def anchor_x(self, value:str):
-        """Anchor to count position from. 
-        \n Relative to self. 
-        \n Possible values: `"left"`, `"center"`, `"right"`
+    def On_Tick(self, delta:float=0.0) -> None:
+        """Called for making some actions for the scene.
+        \n (For calling, use `Tick`)
         """
 
-        self._anchor_x = value
-        self.Validate_anchor_x()
-        self.Math_Collision_Rectangle()       
 
-    def Validate_anchor_x(self):
-        """Validate anchor_x and raise if wrong."""
-
-        if self._anchor_x not in ["left", "center", "right"]: raise TypeError(f"{self.anchor_x} wrong value for anchor_x. Can be \"left\", \"center\" or \"right\"")
-
-
-    @property
-    def anchor_y(self) -> str: return self._anchor_y
-    
-    @anchor_y.setter
-    def anchor_y(self, value:str):
-        """Anchor to count position from. 
-        \n Relative to self. 
-        \n Possible values: `"top"`, `"center"`, `"bottom"`
-        """
-
-        self._anchor_y = value
-        self.Validate_anchor_y()
-        self.Math_Collision_Rectangle()       
-
-    def Validate_anchor_y(self):
-        """Validate anchor_y and raise if wrong."""
-
-        if self._anchor_y not in ["top", "center", "bottom"]: raise TypeError(f"{self.anchor_y} wrong value for anchor_y. Can be \"top\", \"center\" or \"bottom\"")
-
-
-    @property
-    def parent_anchor_x(self) -> str: return self._parent_anchor_x
-    
-    @parent_anchor_x.setter
-    def parent_anchor_x(self, value:str):
-        """Anchor to count position from. 
-        \n Relative to parent_rect. 
-        \n Possible values: `"left"`, `"center"`, `"right"`
-        """
-
-        self._parent_anchor_x = value
-        self.Validate_parent_anchor_x()
-        self.Math_Collision_Rectangle()       
-
-    def Validate_parent_anchor_x(self):
-        """Validate parent_anchor_x and raise if wrong."""
-
-        if self._parent_anchor_x not in ["left", "center", "right"]: raise TypeError(f"{self.parent_anchor_x} wrong value for parent_anchor_x. Can be \"left\", \"center\" or \"right\"")
-    
-
-    @property
-    def parent_anchor_y(self) -> str: return self._parent_anchor_y
-    
-    @parent_anchor_y.setter
-    def parent_anchor_y(self, value:str):
-        """Anchor to count position from. 
-        \n Relatzive to parent_rect. 
-        \n Possible values: `"top"`, `"center"`, `"bottom"`
-        """
-
-        self._parent_anchor_ = value
-        self.Validate_parent_anchor_y()
-        self.Math_Collision_Rectangle()       
-
-    def Validate_parent_anchor_y(self):
-        """Validate parent_anchor_y and raise if wrong."""
-
-        if self._parent_anchor_y not in ["top", "center", "bottom"]: raise TypeError(f"{self.parent_anchor_y} wrong value for parent_anchor_y. Can be \"top\", \"center\" or \"bottom\"")
-          
-
-
-    # ---------- Collision Rect ----------
-    def Math_Collision_Rectangle(self) -> None:
-        """"""
-
-        # get position and scale
-        if type(self._x) in [int, float]: x = self._x
-        if type(self._x) == str: x = self.parent.rect.x * int(self._x[:-1]) / 100
-        if type(self._y) in [int, float]: y = self._y
-        if type(self._y) == str: y = self.parent.rect.y * int(self._y[:-1]) / 100
-        if type(self._width) in [int, float]: width = self._width
-        if type(self._width) == str: width = self.parent.rect.width * int(self._width[:-1]) / 100
-        if type(self._height) in [int, float]: height = self._height
-        if type(self._height) == str: height = self.parent.rect.height * int(self._height[:-1]) / 100
-
-        self.rect.x = x
-        self.rect.y = y
-        self.rect.width = width 
-        self.rect.height = height
-
-        # anchors x
-        # if self._anchor_x == "left":
-        #     if self.__anchor_x == "left": 
-        #         self.rect.left = self.rect.x + self.parent.rect.left
-        #     elif self.__anchor_x == "center": 
-        #         self.rect.left = self.rect.x + self.parent.rect.centerx
-        #     elif self.__anchor_x == "right": 
-        #         self.rect.left = self.rect.x + self.parent.rect.right
-        # elif self._anchor_x == "center":
-        #     if self.__anchor_x == "left": 
-        #         self.rect.centerx = self.rect.x + self.parent.rect.left
-        #     elif self.__anchor_x == "center": 
-        #         self.rect.centerx = self.rect.x + self.parent.rect.centerx
-        #     elif self.__anchor_x == "right": 
-        #         self.rect.centerx = self.rect.x + self.parent.rect.right
-        # elif self._anchor_x == "right":
-        #     if self.__anchor_x == "left": 
-        #         self.rect.right = self.rect.x + self.parent.rect.left
-        #     elif self.__anchor_x == "center": 
-        #         self.rect.right = self.rect.x + self.parent.rect.centerx
-        #     elif self.__anchor_x == "right": 
-        #         self.rect.right = self.rect.x + self.parent.rect.right
-
-        # # anchors y
-        # if self._anchor_y == "top":
-        #     if self.__anchor_y == "top": 
-        #         self.rect.top = self.rect.y + self.parent.rect.top
-        #     elif self.__anchor_y == "center": 
-        #         self.rect.top = self.rect.y + self.parent.rect.centery
-        #     elif self.__anchor_y == "bottom": 
-        #         self.rect.top = self.rect.y + self.parent.rect.bottom
-        # elif self._anchor_y == "center":
-        #     if self.__anchor_y == "top": 
-        #         self.rect.centery = self.rect.y + self.parent.rect.top
-        #     elif self.__anchor_y == "center": 
-        #         self.rect.centery = self.rect.y + self.parent.rect.centery
-        #     elif self.__anchor_y == "bottom": 
-        #         self.rect.centery = self.rect.y + self.parent.rect.bottom
-        # elif self._anchor_y == "bottom":
-        #     if self.__anchor_y == "top": 
-        #         self.rect.bottom = self.rect.y + self.parent.rect.top
-        #     elif self.__anchor_y == "center": 
-        #         self.rect.bottom = self.rect.y + self.parent.rect.centery
-        #     elif self.__anchor_y == "bottom": 
-        #         self.rect.bottom = self.rect.y + self.parent.rect.bottom
-
-    def On_Start(self):
-        """"""
-
-    def Handle(self, event:pygame.event.Event) -> None:
-        """"""
-
-
+    # -----RENDER-----
     def Render(self, target:pygame.Surface) -> None:
-        """Blit element on target"""
+        """Renders this scene's content on `target` pygame.Surface.
+        \n If this scene is closed, does nothing and returns `False`.
+        \n (For codding, use `On_Render`)
+        """
+        
+        # if closed
+        if not self.exists: return False
+
+        # if opened
+        self.On_Render(target)
+        return True
+
+
+    def On_Render(self, target:pygame.Surface) -> None:
+        """Called to render this scene's content on `target` surface.
+        \n (For calling, use `Render`)
+        """
+
+
+    # -----HANDLE-----
+    def Handle(self, event:pygame.event.Event) -> None:
+        """Handles pygame's `event` for this scene.
+        \n If this scene is closed, does nothing and returns `False`.
+        \n (For codding, use `On_Handle`)
+        """
+        
+        # if closed
+        if not self.exists: return False
+
+        # if opened
+        self.On_Handle(event)
+        return True
+
+
+    def On_Handle(self, event:pygame.event.Event) -> None:
+        """Called to handle given pygame event.
+        \n (For calling, use `Handle`)
+        """
+        
